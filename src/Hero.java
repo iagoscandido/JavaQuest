5e5baeb3dd0ca55c3f4bfb7668eee32c530152bc
iago <iago.scandido3@gmail.com>
2026-01-06 02:15:33 -0300

    implement special attack and refactor potion consumption logic

--- src/Hero.java
+++ src/Hero.java
@@ -2,16 +2,23 @@ public class Hero extends Entity {
     private int currentExperiencePoints;
     private int experiencePointsToLevelUp;
     private int potions;
+    private final int maximumPotions;
 
-    public Hero(String name, int healthPoints, int strength, int defense) {
-        super(name, healthPoints, strength, defense, 1);
+    public Hero(String name) {
+        super(name, 10, 5, 2, 1, 20);
         this.currentExperiencePoints = 0;
         this.experiencePointsToLevelUp = 10;
         this.potions = 3;
+        this.maximumPotions = potions;
+
     }
 
     public int getPotions() {
         return potions;
+    }
+
+    public int getMaximumPotions() {
+        return maximumPotions;
     }
 
     public void gainExperiencePoints(int amount) {
@@ -35,34 +42,77 @@ public class Hero extends Entity {
         this.currentExperiencePoints -= this.experiencePointsToLevelUp;
         this.experiencePointsToLevelUp = (int) Math.round(this.experiencePointsToLevelUp * 1.5);
 
-        System.out.printf("PARABÉNS! Você subiu para o nível %d!%n", this.getLevel());
+        String levelUpMessage = String.format("PARABÉNS! Você subiu para o nível %d!%n", this.getLevel());
+        System.out.println(levelUpMessage);
     }
 
-    public void drinkPotion() {
-        int previousHealthPoints = this.healthPoints;
+    public boolean drinkPotion() {
+        int currentHealthPoints = this.healthPoints;
+        boolean isPotionAvailable = this.potions > 0;
+        boolean isMissingHealthPoints = currentHealthPoints < this.maximumHealthPoints;
+        boolean isPotionUseValid = this.isAlive() && isMissingHealthPoints;
 
-        if (this.potions < 1) {
-            System.out.println("Você não tem mais poções!");
+        if (!isPotionAvailable) {
+            String noPotionsAvailableMessage = "Você não tem mais poções!";
+            System.out.println(noPotionsAvailableMessage);
+            return false;
         }
 
-        if (isHealValid() && this.potions > 0) {
-            this.potions--;
-            this.healthPoints = Math.min(this.healthPoints + 10, this.maximumHealthPoints);
-            int healPoints = this.healthPoints - previousHealthPoints;
-            System.out.printf("\n%s consumiu a poção e curou %d\n", this.getName(), healPoints);
+        if (!isMissingHealthPoints) {
+            String noMissingHealthMessage = "Seu HP já está cheio! Poção não utilizada.";
+            System.out.println(noMissingHealthMessage);
+            return false;
         }
-    }
 
-    public boolean isHealValid() {
-        return this.isAlive() && this.healthPoints < this.maximumHealthPoints;
+        if (!isPotionUseValid) {
+            return false;
+        }
+
+        this.potions--;
+        this.healthPoints = Math.min(this.healthPoints + 10, this.maximumHealthPoints);
+        int healthPointsRestored = this.healthPoints - currentHealthPoints;
+
+        String potionUsedMessage = String.format("%s curou: %d", this.getName(), healthPointsRestored);
+
+        System.out.println(potionUsedMessage);
+
+        return true;
     }
 
     @Override
     public String displayStatus() {
-        return String.format("Nome: %s | Nível: %d | HP: %d/%d | Força: %d | Defesa: %d | Poções: %d)", this.getName(),
+        return String.format("Nome: %s | Nível: %d | HP: %d/%d | Força: %d | Defesa: %d | Energia %d/%d |Poções: %d/%d",
+                this.getName(),
                 this.getLevel(),
-                this.getHealthPoints(), this.getMaximumHealthPoints(), this.getStrength(), this.getDefense(),
-                this.getPotions() );
+                this.getHealthPoints(), this.getMaximumHealthPoints(),
+                this.getStrength(),
+                this.getDefense(),
+                this.getEnergyPoints(), this.getMaximumEnergyPoints(),
+                this.getPotions(), this.getMaximumPotions());
+    }
+
+    public boolean specialAttack(Entity target) {
+        int energyCost = 10;
+        String notEnoughEnergyToAttackMessage = String.format("""
+                Energia insuficiente para realizar o ataque especial
+                energia necessária: %d | energia atual: %d
+                """, energyCost, this.energyPoints);
+
+        if (this.energyPoints < energyCost) {
+            System.out.println(notEnoughEnergyToAttackMessage);
+            return false;
+        }
+
+        int damage = this.strength * 2;
+        int updatedEnergyPoints = this.energyPoints -= energyCost;
+        String specialAttackMessage = "Você realizou um ataque especial";
+        String updatedEnergyPointsMessage = String.format("Energia atual: %d/%d", updatedEnergyPoints,
+                this.getMaximumEnergyPoints());
+
+        System.out.println(specialAttackMessage);
+        System.out.println(updatedEnergyPointsMessage);
+        target.takeDamage(damage);
+        return true;
     }
 
 }
