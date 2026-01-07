ce302a80bce788ed766b0f9002828f946d9107aa
iago <iago.scandido3@gmail.com>
2026-01-07 15:42:50 -0300

    feat: implement D20 dice roll, creature armour class, improve attack logic and feedback. add testes for new features

--- src/main/java/br/com/sciago/Creature.java
+++ src/main/java/br/com/sciago/Creature.java
@@ -5,12 +5,18 @@ public class Creature {
     private final AbilityScores abilityScores;
     private int maxHp;
     private int currentHp;
+    private int armourClass;
 
     public Creature(String name, AbilityScores abilityScores) {
         this.name = name;
         this.abilityScores = abilityScores;
-        this.currentHp = calculateMaxHp(abilityScores.constitutionModifier());
-        this.maxHp = currentHp;
+        this.maxHp = calculateMaxHp(abilityScores.constitutionModifier());
+        this.currentHp = maxHp;
+        this.armourClass = calculateArmourClass(abilityScores.dexterityModifier());
+    }
+
+    public int getArmourClass() {
+        return armourClass;
     }
 
     public int getMaxHp() {
@@ -21,8 +27,8 @@ public class Creature {
         return currentHp;
     }
 
-    private int calculateMaxHp(int constitutionModifier) {
-        int maxHp = 10 + constitutionModifier;
+    private int calculateMaxHp(int modifier) {
+        int maxHp = 10 + modifier;
 
         if (maxHp <= 0)
             throw new IllegalArgumentException("[%d] hp value is invalid. hp must be an positive integer."
@@ -31,8 +37,50 @@ public class Creature {
         return maxHp;
     }
 
+    private int calculateArmourClass(int modifier) {
+        return 10 + modifier;
+    }
+
+    public void attack(Creature target) {
+        String attackMessage = "%s attacks %s".formatted(this.name, target.name);
+        System.out.println(attackMessage);
+        int rollAttackDice = Dice.rollD20();
+        int attackScore = rollAttackDice + abilityScores.strengthModifier();
+
+        String attackScoreMessage = "attack score: %d (D20: %d + Mod: %d)".formatted(attackScore, rollAttackDice,
+                abilityScores.strengthModifier());
+        System.out.println(attackScoreMessage);
+
+        if (attackScore >= target.getArmourClass()) {
+            String successMessage = "%s hits %s".formatted(this.name, target.name);
+            System.out.println(successMessage);
+
+            int damageDealt = unarmedAttack(abilityScores.strengthModifier());
+            target.takeDamage(damageDealt);
+
+        } else {
+            String missMessage = "%s misses %s [AC: %d]".formatted(this.name, target.name, target.getArmourClass());
+            System.out.println(missMessage);
+        }
+    }
+
+    public int unarmedAttack(int damageModifier) {
+        int damage = calculateUnarmedAttack(damageModifier);
+        if (damage < 1) damage = 1;
+        return damage;
+    }
+
+    private int calculateUnarmedAttack(int damageModifier) {
+        return Math.max(0, 1 + damageModifier);
+    }
+
     public void takeDamage(int damage) {
-        this.currentHp = Math.max(0, this.currentHp - damage);
+        int rawDamage = Math.max(0, damage);
+        int damageTaken = this.currentHp - rawDamage;
+        this.currentHp = Math.clamp(damageTaken, 0, this.maxHp);
+
+        String message = "%s takes damage: %d | HP: %d/%d".formatted(this.name, damageTaken, this.currentHp, this.maxHp);
+        System.out.println(message);
     }
 
     public boolean isBloodied() {
@@ -41,11 +89,11 @@ public class Creature {
 
     public String getSummary() {
         return """
-                 %s | HP: %d/%d
+                 %s | HP: %d/%d | AC: %d
                 STR: %d (%+d) | DEX: %d (%+d) | CON: %d (%+d)
                 INT: %d (%+d) | WIS: %d (%+d) | CHA: %d (%+d)
                 """.formatted(
-                name, this.getCurrentHp(), this.getMaxHp(),
+                name, this.getCurrentHp(), this.getMaxHp(), this.getArmourClass(),
                 abilityScores.strength(), abilityScores.strengthModifier(),
                 abilityScores.dexterity(), abilityScores.dexterityModifier(),
                 abilityScores.constitution(), abilityScores.constitutionModifier(),
--- src/test/java/CreatureTest.java
+++ src/test/java/CreatureTest.java
@@ -1,6 +1,5 @@
 import br.com.sciago.AbilityScores;
 import br.com.sciago.Creature;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +8,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreatureTest {
     private AbilityScores defaultScores;
@@ -21,7 +23,7 @@ public class CreatureTest {
     @BeforeEach
     void setUp() {
         defaultScores = new AbilityScores(10, 10, 10, 10, 10, 10);
-        defaultCreature = new Creature("Grog", defaultScores);
+        defaultCreature = new Creature("Bob", defaultScores);
     }
 
     @ParameterizedTest
@@ -34,7 +36,7 @@ public class CreatureTest {
         int expectedMaxHp = 10 + scores.constitutionModifier();
         int actualMaxHp = creature.getMaxHp();
 
-        Assertions.assertEquals(expectedMaxHp, actualMaxHp,
+        assertEquals(expectedMaxHp, actualMaxHp,
                 () -> "failure to score: " + conScore + " (modifier: " + scores.constitutionModifier() + ")");
     }
 
@@ -44,7 +46,7 @@ public class CreatureTest {
         int expectedCurrentHp = 10 + defaultScores.constitutionModifier();
         int actualCurrentHp = defaultCreature.getCurrentHp();
 
-        Assertions.assertEquals(expectedCurrentHp,
+        assertEquals(expectedCurrentHp,
                 actualCurrentHp,
                 "currentHp should be equals to expectedCurrentHp");
     }
@@ -59,7 +61,7 @@ public class CreatureTest {
 
         int actualCurrentHp = defaultCreature.getCurrentHp();
 
-        Assertions.assertEquals(expectedCurrentHp, actualCurrentHp);
+        assertEquals(expectedCurrentHp, actualCurrentHp);
     }
 
     @Test
@@ -73,16 +75,53 @@ public class CreatureTest {
 
         int actualCurrentHp = defaultCreature.getCurrentHp();
 
-        Assertions.assertEquals(expectedCurrentHp, actualCurrentHp);
+        assertEquals(expectedCurrentHp, actualCurrentHp);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {6, 7, 8, 9, 10, 11})
-    public void shouldReturnTrueIfCurrentHpLessOrEqualThanHalfMaxHp(int damage) {
+    @DisplayName("Should set isBloodied to true if Hp is half or less")
+    public void shouldSetIsBloodiedIfHpIsHalfOrLess(int damage) {
 
         defaultCreature.takeDamage(damage);
 
-        Assertions.assertTrue(defaultCreature.isBloodied());
+        assertTrue(defaultCreature.isBloodied());
 
     }
+
+    @ParameterizedTest
+    @MethodSource("abilityScoreRange")
+    @DisplayName("Should calculate AC Correctly")
+    public void shouldCalculateACCorrectly(int dexScore) {
+        AbilityScores scores = new AbilityScores(10, dexScore, 10, 10, 10, 10);
+        Creature creature = new Creature("Test Creature", scores);
+
+        int expectedAC = 10 + scores.dexterityModifier();
+        int currentAC = creature.getArmourClass();
+
+        assertEquals(expectedAC, currentAC);
+    }
+
+    @Test
+    @DisplayName("A successful attack should reduce target HP")
+    void successfulAttackShouldReduceTargetHp() {
+        Creature attacker = new Creature("attacker",
+                new AbilityScores(20, 10, 10, 10, 10, 10));
+        Creature target = new Creature("target",
+                new AbilityScores(10, 1, 10, 10, 10, 10));
+
+        int initialHp = target.getCurrentHp();
+        attacker.attack(target);
+
+        assertTrue(target.getCurrentHp() < initialHp, "Target HP should have been reduced.");
+    }
+
+    @Test
+    @DisplayName("Should set HP to zero when damage is fatal")
+    void shouldSetHPToZeroWhenDamageIsFatal() {
+        defaultCreature.takeDamage(100);
+
+        assertEquals(0, defaultCreature.getCurrentHp(), "hp should be zero when damage is fatal");
+    }
+
 }
