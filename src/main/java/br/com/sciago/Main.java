4e5745964511fd3a5f9233e822c7981b533839d9
iago <iago.scandido3@gmail.com>
2026-01-07 16:01:45 -0300

    feat: finalize combat loop with proper death checks and turn resolution

--- src/main/java/br/com/sciago/Main.java
+++ src/main/java/br/com/sciago/Main.java
@@ -1,98 +1,39 @@
 package br.com.sciago;
 
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        Scanner sc = new Scanner(System.in);
-//    System.out.print("Informe seu nome: ");
-//    String playerName = sc.nextLine();
+        AbilityScores bobScores = new AbilityScores(Dice.rollD20(), Dice.rollD20(), Dice.rollD20(), Dice.rollD20(), Dice.rollD20(), Dice.rollD20());
+        Creature bob = new Creature("bob", bobScores);
 
+        AbilityScores slimeScores = new AbilityScores(Dice.rollD20(), Dice.rollD20(), Dice.rollD20(), Dice.rollD20(), Dice.rollD20(), Dice.rollD20());
+        Creature slime = new Creature("slime", slimeScores);
 
-        String monsterSpawnedSuccessMessage = "Monstro surgiu!";
-        String combatStartMessage = "Combate iniciado!";
-        String combatEndMessage = "Combate finalizado!";
+        System.out.println("Bob: " + bob.getSummary());
+        System.out.println("Slime: " + slime.getSummary());
 
+        int turnsCount = 0;
+        while (bob.getCurrentHp() > 0 && slime.getCurrentHp() > 0) {
+            turnsCount++;
 
-        String playerName = "testeName";
-
-        Hero player = new Hero(playerName);
-        String characterCreatedMessage = String.format("Personagem: %s criado com sucesso!", player.getName());
-        System.out.println(characterCreatedMessage);
-
-        Monster monster = Monster.spawn(player.getLevel());
-
-        System.out.println(monsterSpawnedSuccessMessage);
-        System.out.println(monster.displayStatus());
-
-        int turn = 0;
-        System.out.println(combatStartMessage);
-        while (player.isAlive() && monster.isAlive()) {
-            turn++;
-            String currentTurnMessage = String.format("Turno: %d", turn);
-            String endTurnMessage = "Fim do turno";
-            String optionsMessage = "Ações: 1 - Atacar | 2 - Beber poção | 3 - Ataque Especial (10 EP)";
-            String monsterDefeatedMessage = String.format("%s derrotado! Você recebeu %d Pontos de Experiencia",
-                    monster.getName(), monster.getExperiencePointsReward());
-            String playerDefeatedMessage = String.format("Você foi derrotado por: %s", monster.getName());
-
-            System.out.println(currentTurnMessage);
-            System.out.println("Jogador: " + player.displayStatus());
-            System.out.println("Monstro: " + monster.displayStatus());
-            System.out.println();
-            System.out.println("Turno do Jogador");
-
-            boolean isTurnEnded = false;
-            while (!isTurnEnded) {
-
-                System.out.println(optionsMessage);
-                int choice = sc.nextInt();
-
-                while (choice != 1 && choice != 2 && choice != 3) {
-                    System.out.println("Opção inválida.");
-                    System.out.println(optionsMessage);
-                    choice = sc.nextInt();
-                }
-
-                switch (choice) {
-                    case 1:
-                        player.attack(monster);
-                        isTurnEnded = true;
-                        break;
-                    case 2:
-                        if (player.drinkPotion()) isTurnEnded = true;
-                        break;
-                    case 3:
-                        if (player.specialAttack(monster)) isTurnEnded = true;
-                        break;
-                }
-            }
-
-            if (!monster.isAlive()) {
-                player.gainExperiencePoints(monster.getExperiencePointsReward());
-                System.out.printf(monsterDefeatedMessage);
-                System.out.println(combatEndMessage);
+            bob.attack(slime);
+            if (slime.getCurrentHp() == 0) {
+                System.out.println("Bob Wins");
                 break;
             }
 
-            System.out.println("Turno do Monstro");
-            monster.attack(player);
-
-            if (!player.isAlive()) {
-                System.out.println(playerDefeatedMessage);
-                System.out.println(combatEndMessage);
+            slime.attack(bob);
+            if (bob.getCurrentHp() == 0) {
+                System.out.println("Slime Wins");
                 break;
             }
 
-//        regeneration phase
-            player.regenerateEnergyPoints();
-            monster.regenerateEnergyPoints();
+            System.out.println("Bob: " + bob.getSummary());
+            System.out.println("Slime: " + slime.getSummary());
 
-            System.out.println(endTurnMessage);
         }
-
-        sc.close();
+        System.out.println("======= COMBAT SUMMARY =======");
+        System.out.println("Bob: " + bob.getSummary());
+        System.out.println("Slime: " + slime.getSummary());
+        System.out.println("Total turns: " + turnsCount);
     }
 }
